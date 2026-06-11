997c2d6d05bdd3e91be4c7df740a33b43bd36409
Christian Praß <mail@prass.tech>
2026-06-12 00:39:12 +0200

    client: create a login wrapper that starts a new session

--- src/client/src/App.tsx
+++ src/client/src/App.tsx
@@ -1,25 +1,28 @@
 import type { ReactElement } from 'react';
-import {BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import NavBar from './NavBar';
 import AboutUs from './AboutUs';
 import Dashboard from './Dashboard';
 import Scan from './Scan';
 import Quest from './Quest';
+import LoginWrapper from './LoginWrapper';
 
 function App(): ReactElement {
   return (
     <BrowserRouter>
-      <NavBar>
+      <LoginWrapper>
+        <NavBar>
           <Routes>
-              <Route path="/" element={<Dashboard />} />
-              <Route path="/scan" element={<Scan />} />
-              <Route path="/quest/:questId" element={<Quest />} />
-              <Route path="about" element={<AboutUs />} />
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/scan" element={<Scan />} />
+            <Route path="/quest/:questId" element={<Quest />} />
+            <Route path="about" element={<AboutUs />} />
           </Routes>
-      </NavBar>
+        </NavBar>
+      </LoginWrapper>
     </BrowserRouter>
-     
+
   );
 }
 
