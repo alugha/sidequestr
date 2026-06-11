fe87bcb913df13f2b6ba2c3f2598e58874db7b7c
Christian Praß <mail@prass.tech>
2026-06-11 22:01:35 +0200

    server: add api to fetch a single quest

--- src/server/routes/api/quest.ts
+++ src/server/routes/api/quest.ts
@@ -7,16 +7,19 @@ import { QuestSchema } from '../../../shared/schemas/quest';
 
 const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
   fastify.get(
-    '/quests',
+    '/quest/:id',
     {
       schema: {
+        params: Type.Object({
+          id: Type.String({ minLength: 1 }),
+        }),
         response: {
-          200: Type.Array(QuestSchema),
+          200: QuestSchema,
         },
       }
     },
-    async function () {
-      return this.dl.getAllQuests();
+    async function (request) {
+      return this.dl.getQuestById(request.params.id)
     }
   )
 }
