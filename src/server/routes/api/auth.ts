39e254743c1f4280c490c8eb8d6504d1925c7ea5
Benjamin Sparks <b.sparks@alugha.com>
2026-06-11 22:37:32 +0200

    fix: keep to typebox schema definitions

--- src/server/routes/api/auth.ts
+++ src/server/routes/api/auth.ts
@@ -1,31 +1,32 @@
 import {
   type FastifyPluginAsyncTypebox,
-  Type
-} from '@fastify/type-provider-typebox'
-import { UserSchema } from '../../../shared/schemas/user.ts'
+  Type,
+} from "@fastify/type-provider-typebox";
+import { UserSchema } from "../../../shared/schemas/user.ts";
 
 const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
   fastify.get(
-    '/auth',
+    "/auth",
     {
       schema: {
         response: {
           200: UserSchema,
           404: Type.Object({
-            error: Type.String()
-          })
+            error: Type.String(),
+          }),
         },
-      }
+      },
     },
     async function (request, reply) {
       if (!request.session.user) {
-        reply.status(404);
-        return { error: "unauthorized" }
+        return reply.status(404).send({ error: "unauthorized" });
       }
 
-      return { name: request.session.user.name };
-    }
-  )
-}
+      return reply
+        .status(200)
+        .send({ id: request.session.user.id, name: request.session.user.name });
+    },
+  );
+};
 
-export default plugin
+export default plugin;
