3145c98d2e1f4475a12456a42823368fd61e6529
Benjamin Sparks <b.sparks@alugha.com>
2026-06-11 22:37:32 +0200

    fix: keep to typebox schema definitions

--- src/server/routes/api/login.ts
+++ src/server/routes/api/login.ts
@@ -1,41 +1,39 @@
-import { randomUUID } from 'node:crypto'
+import { randomUUID } from "node:crypto";
 import {
   type FastifyPluginAsyncTypebox,
-  Type
-} from '@fastify/type-provider-typebox'
-import { LoginSchema } from '../../../shared/schemas/auth.ts'
-import { UserSchema } from '../../../shared/schemas/user.ts'
+  Type,
+} from "@fastify/type-provider-typebox";
+import { LoginSchema } from "../../../shared/schemas/auth.ts";
+import { UserSchema } from "../../../shared/schemas/user.ts";
 
 const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
   fastify.post(
-    '/login',
+    "/login",
     {
       schema: {
         body: LoginSchema,
         response: {
           200: UserSchema,
           400: Type.Object({
-            error: Type.String()
-          })
+            error: Type.String(),
+          }),
         },
-      }
+      },
     },
     async function (request, reply) {
       if (request.session.user) {
-        reply.status(400);
-        return { error: "already logged in" }
+        return reply.status(400).send({ error: "already logged in" });
       }
 
-      const { name } = request.body
+      const id = randomUUID();
+      const { name } = request.body;
 
-      request.session.user = { id: randomUUID(), name };
+      request.session.user = { id, name };
       await request.session.save();
 
-      return {
-        name
-      }
-    }
-  )
-}
+      return reply.status(200).send({ id, name });
+    },
+  );
+};
 
-export default plugin
+export default plugin;
