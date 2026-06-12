a6b606f31286f080ed8df5d59fe7d7fcbd993319
Christian Praß <mail@prass.tech>
2026-06-12 12:14:35 +0200

    server: fix CSP settings for making camera input work

--- src/server/plugins/external/helmet.ts
+++ src/server/plugins/external/helmet.ts
@@ -1,7 +1,15 @@
-import helmet from '@fastify/helmet'
+import helmet, { type FastifyHelmetOptions } from '@fastify/helmet'
 
-export const autoConfig = {
-  // Set plugin options here
+export const autoConfig: FastifyHelmetOptions = {
+  contentSecurityPolicy: {
+    directives: {
+      imgSrc: ["'self'", "data:", "blob:"],
+      mediaSrc: ["'self'", "blob:", "data:"],
+      workerSrc: ["'self'", "blob:"],
+      scriptSrc: ["'self'", "'wasm-unsafe-eval'", "https://fastly.jsdelivr.net"],
+      connectSrc: ["'self'", "https://fastly.jsdelivr.net"],
+    },
+  }
 }
 
 export default helmet
