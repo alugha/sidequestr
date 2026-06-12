8907a61f4db12fbaa272095362df8cf95ac7720b
Christian Praß <mail@prass.tech>
2026-06-12 11:39:38 +0200

    client: ask for device permission

--- src/client/src/Scan.tsx
+++ src/client/src/Scan.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useState  } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import './App.css';
 
 // https://www.npmjs.com/package/@yudiel/react-qr-scanner
@@ -6,44 +6,61 @@ import { Scanner } from '@yudiel/react-qr-scanner';
 import { Button } from '@progress/kendo-react-buttons';
 import { Input } from '@progress/kendo-react-inputs';
 
-const Scan:React.FC = ()=>{
+const Scan: React.FC = () => {
 
-  const [taskId,setTaskId] = useState('');
-  const [success,setSuccess] = useState<null|boolean>(null);
+  const [taskId, setTaskId] = useState('');
+  const [success, setSuccess] = useState<null | boolean>(null);
+  const [_hasPermission, setHasPermission] = useState(false);
 
-    const completeTask = useCallback((taskId:string)=>{
-      fetch(`/api/task/${taskId}`,{method:"PUT"}).then((r)=>{setSuccess(r.ok)}).catch(()=>{setSuccess(false)});
-    },[])
-
-    return (
-    <div style={{margin:"1rem"}}>
-    <h2>Scan a code and keep questing</h2>
-
-    {success === true && <>
-      <h2>Hussah!</h2>
-      <p>You successfully claimed your magical object and made progress. Congratulations. But your journey is not over yet. More stuff to find out there, keep your eyes open!</p>
-      </>
+  const requestCamera = async () => {
+    try {
+      await navigator.mediaDevices.getUserMedia({ video: true });
+      setHasPermission(true);
+    } catch (err) {
+      console.error(err)
     }
-    {success === false && <><h2>Huh?!</h2>
-    <p>It appears that whatever your experiment is about, it is not working. Maybe you are holding it wrong. Stay focused.</p></>}
-    {success !== null ? <Button onClick={()=>setSuccess(null)}>Make another claim</Button> : 
-      <>
-        <div style={{width:"15rem", margin:"0 auto", maxWidth:"calc(100vw - 1rem)"}}>  
-          <Scanner 
+  };
 
-            onScan={(result) => completeTask(result[0].rawValue)}
-            onError={(error) => console.log(error?.message)}
-          />
-            
+  useEffect(() => {
+    void requestCamera();
+  }, []);
+
+
+  const completeTask = useCallback((taskId: string) => {
+    fetch(`/api/task/${taskId}`, { method: "PUT" }).then((r) => { setSuccess(r.ok) }).catch(() => { setSuccess(false) });
+  }, [])
+
+  return (
+    <div style={{ margin: "1rem" }}>
+      <h2>Scan a code and keep questing</h2>
+
+      {success === true && <>
+        <h2>Hussah!</h2>
+        <p>You successfully claimed your magical object and made progress. Congratulations. But your journey is not over yet. More stuff to find out there, keep your eyes open!</p>
+      </>
+      }
+      {success === false && <><h2>Huh?!</h2>
+        <p>It appears that whatever your experiment is about, it is not working. Maybe you are holding it wrong. Stay focused.</p></>}
+      {success !== null ? <Button onClick={() => setSuccess(null)}>Make another claim</Button> :
+        <>
+          <div style={{ width: "15rem", margin: "0 auto", maxWidth: "calc(100vw - 1rem)" }}>
+            <Scanner
+              constraints={{
+
+              }}
+              onScan={(result) => completeTask(result[0].rawValue)}
+              onError={(error) => console.log(error?.message)}
+            />
+
           </div>
           <h3>No camera available?</h3>
-          <form onSubmit={(e)=>{e.preventDefault();completeTask(taskId)}}>
-            <label  >Code<Input type="text" onChange={e=>setTaskId(e.target.value as string)} required/></label> 
-            <Button type="submit" style={{width:'100%'}}>Complete a task</Button>
-          </form>   
+          <form onSubmit={(e) => { e.preventDefault(); completeTask(taskId) }}>
+            <label  >Code<Input type="text" onChange={e => setTaskId(e.target.value as string)} required /></label>
+            <Button type="submit" style={{ width: '100%' }}>Complete a task</Button>
+          </form>
         </>}
-    
-  </div>
+
+    </div>
   );
 }
 
